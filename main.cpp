1b0afa6cf9cb7e5dce6c9df0c54f198f6f3c9855
davidegaribaldi <100833454+davidegaribaldi@users.noreply.github.com>
2022-04-01 21:43:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n = 0, a = 1, b = 1,c=0;
+    cin >> n;
+    if(n>2)
+    {
+        cout << a << endl;
+        cout << b << endl;
+        for (int i = 0; i < (n-2); i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+            cout<<c<<endl;
+        }
+    }else
+    {
+        cout<<"errore";
+    }
+
+    return 0;
 }
